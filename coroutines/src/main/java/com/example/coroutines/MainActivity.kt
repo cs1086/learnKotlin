a18f99bfb9d22672a7da8d7e99522afb7f4cbaf8
cs1086 <xtnact541@gmail.com>
2022-03-12 21:45:59 +0800

    SwipeRefreshLayout 下拉時refresh顯示loading圖示的實作

--- coroutines/src/main/java/com/example/coroutines/MainActivity.kt
+++ coroutines/src/main/java/com/example/coroutines/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.widget.Button
 import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
+import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
     lateinit var coroutineScope: CoroutineScope
@@ -25,15 +26,17 @@ class MainActivity : AppCompatActivity() {
         coroutineScope.launch(Dispatchers.Main) {
 
             println("####執行countNumber之前"+Thread.currentThread().name)
-            async {countNumber(10)}
-            delay(5000)
-            async {countNumber2(10)}
-
+//            async {countNumber(10)}
+//            delay(5000)
+//            async {countNumber2(10)}
+            val a =async {getString2(10)}
+            val b =async {getString2(5)}
+            println("####得到,b=${b.await()}")
+            println("####得到a=${a.await()}")
             println("####執行countNumber之後"+Thread.currentThread().name)
+
         }
-
         println("####執行launch之後"+Thread.currentThread().name)
-
     }
     suspend fun countNumber(time:Int)= withContext(Dispatchers.Default){
         var i=0
@@ -44,18 +47,12 @@ class MainActivity : AppCompatActivity() {
 
         }
     }
-    suspend fun countNumber2(time:Int)= withContext(Dispatchers.IO){
-        
-        //a1
-        //sl
-        //a2
 
-        var i=0
-        while(i++ < time){
-            println("####countNumber2現在數字=$i,"+Thread.currentThread().name)
-            changeUi(i.toString())
-            Thread.sleep(1000)
-        }
+    suspend fun getString2(time:Long):String = withContext(Dispatchers.Default){
+        println("####getString21")
+        delay(time*1000)
+        println("####getString22")
+        (time+1).toString()
     }
     suspend fun changeUi(text:String)= withContext(Dispatchers.Main){
         button.setText(text)
