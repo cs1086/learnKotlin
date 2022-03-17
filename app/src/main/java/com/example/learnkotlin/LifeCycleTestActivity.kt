f38864a90816554352563069d23b9e5bc86c7dc3
cs1086 <xtnact541@gmail.com>
2022-03-17 23:30:52 +0800

    生命週期的測試2.0

--- app/src/main/java/com/example/learnkotlin/LifeCycleTestActivity.kt
+++ app/src/main/java/com/example/learnkotlin/LifeCycleTestActivity.kt
@@ -1,22 +1,31 @@
 package com.example.learnkotlin
 
+import android.content.Intent
 import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 
 class LifeCycleTestActivity : AppCompatActivity() {
     var type=FragType.BLANK1
     var fragmentManager=this.supportFragmentManager
     lateinit var button: Button
+    lateinit var button2: Button
     lateinit var transition:FragmentTransaction
-    val blankFragment1 = BlankFragment1()
-    val blankFragment2 = BlankFragment2()
+    lateinit var blankFragment1:Fragment
+    lateinit var blankFragment2:Fragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_life_cycle_test)
         println("####Activity1.onCreate")
+        button2=findViewById(R.id.button4)
+        val intent = Intent(this,LifeCycleTest2Activity::class.java)
+        button2.setOnClickListener{
+            startActivity(intent)
+            this.finish()
+        }
         button=findViewById(R.id.fragButton)
         button.setOnClickListener{
             transition=fragmentManager.beginTransaction().setCustomAnimations(
@@ -25,6 +34,7 @@ class LifeCycleTestActivity : AppCompatActivity() {
             when(type){
                 FragType.BLANK1->{
                     type=FragType.BLANK2
+
                     if(blankFragment2.isAdded) {//檢查這個fragment是否已經被加入
                         transition.hide(blankFragment1).show(blankFragment2).commit()
                     }else{
@@ -40,10 +50,19 @@ class LifeCycleTestActivity : AppCompatActivity() {
                     }
                 }
             }
+            transition.addToBackStack(null)
         }
         transition=fragmentManager.beginTransaction().setCustomAnimations(
             android.R.anim.fade_in, R.anim.fragment_close_exit)
-        transition.add(R.id.fragTab, blankFragment1).commit()
+        if(savedInstanceState!=null) {
+            blankFragment1 = fragmentManager.getFragment(savedInstanceState, "f1") ?: BlankFragment1()
+            blankFragment2 = fragmentManager.getFragment(savedInstanceState, "f2") ?: BlankFragment2()
+        }else{
+            blankFragment1 = BlankFragment1()
+            blankFragment2 = BlankFragment2()
+        }
+        if(!blankFragment1.isAdded)transition.add(R.id.fragTab, blankFragment1).commit()
+        transition.addToBackStack(null)
     }
         enum class FragType{
         BLANK1,
@@ -55,6 +74,8 @@ class LifeCycleTestActivity : AppCompatActivity() {
     }
     override fun onSaveInstanceState(outState: Bundle) {
         println("####Activity1.onSaveInstanceState")
+        fragmentManager.putFragment(outState,"f1",blankFragment1)
+        if(this::blankFragment2.isInitialized && blankFragment2.isAdded)fragmentManager.putFragment(outState,"f2",blankFragment2)
         super.onSaveInstanceState(outState)
     }
     override fun onResume() {
@@ -71,6 +92,7 @@ class LifeCycleTestActivity : AppCompatActivity() {
     }
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         println("####Activity1.onRestoreInstanceState")
+
         super.onRestoreInstanceState(savedInstanceState)
     }
     override fun onDestroy() {
