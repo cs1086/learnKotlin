8c1b3348283f318f5278e74be27425d9431c8dbf
cs1086 <xtnact541@gmail.com>
2022-03-25 21:05:43 +0800

    研究workManager 2.0

--- app/src/main/java/com/example/learnkotlin/MyWork.kt
+++ app/src/main/java/com/example/learnkotlin/MyWork.kt
@@ -16,3 +16,14 @@ class MyWork(context: Context,params: WorkerParameters): Worker(context,params) 
         return Result.success(outPutData)
     }
 }
+class MyWork2(context: Context,params: WorkerParameters): Worker(context,params) {
+    override fun doWork(): Result {
+        println("####MyWork2.doWork${Thread.currentThread()}")
+        var i=0
+        while(i<inputData.getInt("time",10)){println("####MyWork2=${i++}")
+            Thread.sleep(1000)
+        }
+        val outPutData=workDataOf("data" to "王柏榕")
+        return Result.success(outPutData)
+    }
+}
